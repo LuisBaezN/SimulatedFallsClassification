0d4505515241ee89665ac94c79036ffc6c0cfdd2
LuisBaezN <baez.201041081@gmail.com>
2022-04-17 16:40:29 -0500

    Minor changes

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# SimulatedFallsClassification
+# Classification of Simulated Falls 
+
+This repo shows how to use an artifitial neural network to classify falls. In order to accomplish this, we used the data set "Simulated Falls and Daily Living Activities Dataset" provided by Ahmet Turan Özdemir and Billur Barshan.
+
+The relevant information about this data si listed below:
+
+* [17 Volunteers × Avg. 5 repetitions × 36 Movements]
+* [36 Movements including 20 Falls and 16 Daily Living Activities 5 Sensors each includes 3 axis Accelerometer, Gyroscope and Magnetometer.]
+* [Attribute Information: Xsens MTw Motion Tracking Kit]
+
+## Technologies
+
+We used the following languages to build the solution:
+
+* [R]
+* [Python]
+* [Tensorflow]
+
+Further information about the versions is included in the environment file.
+
+## Launch
+
+To extract the raw data from the data set an algorithm was implemented in R. This algorithm just extract two classes, and clean the data. So, tu run this example, firs run the Fall_Deep file to extract the data, and then, run ann file to build and predict.
+
+The architecture of the artifitial neural network is : 300/150/70/1, with a sigmoid function at the end of the net. 
--- ann.py
+++ ann.py
@@ -1,8 +1,10 @@
+from gc import callbacks
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 def norm(x, min, max):
@@ -10,12 +12,13 @@ def norm(x, min, max):
     s_scaled = x_std*(1 - 0) + 0
     return s_scaled
 
-def build_model():
+def build_model(in_shape):
+    neurons=[300,150,70,1]
     model = keras.Sequential([
-        layers.Dense(300, activation="relu", input_shape=[270]),
-        layers.Dense(150, activation="relu"),
-        layers.Dense(70, activation="relu"),
-        layers.Dense(1, activation="sigmoid")
+        layers.Dense(neurons[0], activation="relu", input_shape=[in_shape]),
+        layers.Dense(neurons[1], activation="relu"),
+        layers.Dense(neurons[2], activation="relu"),
+        layers.Dense(neurons[3], activation="sigmoid")
     ])
 
     optimizer = tf.keras.optimizers.RMSprop(0.001)
@@ -24,6 +27,31 @@ def build_model():
                     optimizer=optimizer,
                     metrics=['mae', 'mse'])
     return model
+
+def plot_history(history):
+  hist = pd.DataFrame(history.history)
+  hist['epoch'] = history.epoch
+
+  plt.figure()
+  plt.xlabel('Epoch')
+  plt.ylabel('Mean Abs Error [MPG]')
+  plt.plot(hist['epoch'], hist['mae'],
+           label='Train Error')
+  plt.plot(hist['epoch'], hist['val_mae'],
+           label = 'Val Error')
+  plt.ylim([0,5])
+  plt.legend()
+
+  plt.figure()
+  plt.xlabel('Epoch')
+  plt.ylabel('Mean Square Error [$MPG^2$]')
+  plt.plot(hist['epoch'], hist['mse'],
+           label='Train Error')
+  plt.plot(hist['epoch'], hist['val_mse'],
+           label = 'Val Error')
+  plt.ylim([0,20])
+  plt.legend()
+  plt.show()
 
 if __name__ == "__main__":
 
@@ -79,7 +107,7 @@ if __name__ == "__main__":
     
     #///////////////////////// Regression Model //////////////////////
 
-    model = build_model()
+    model = build_model(data_ent_norm.shape[1])
 
     model.summary()
 
@@ -88,11 +116,14 @@ if __name__ == "__main__":
     example_result = model.predict(example_batch)
     print(example_result)
 
-    epocas = 7    
+    epocas = 17    
 
-    history = model.fit(
+    early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
+
+    training_h = model.fit(
         data_ent_norm, lab_ent,
-        epochs=epocas, validation_split = 0.2, verbose=1)
+        epochs=epocas, validation_split = 0.2, verbose=1,
+        callbacks=[early_stop])
 
     #//////////////////////////// Results /////////////////////////////
 
@@ -100,3 +131,7 @@ if __name__ == "__main__":
 
     print(valid_predictions)
     print(lab_val)
+
+    #/////////////////////////// Training history /////////////////////
+
+    plot_history(training_h)
